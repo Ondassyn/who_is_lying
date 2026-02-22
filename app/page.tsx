ec848a00213552b32fa5432e98c2173fba29d32a
Ondassyn <oabdrakhmanov@alumni.nu.edu.kz>
2026-02-22 23:27:43 +0500

    restores logo

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,11 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col gap-8 justify-center items-center p-4">
       <Image
-        src="/logo.png" // Path relative to the public directory
-        alt="Description of my local image"
-        width={300} // Intrinsic width of the image in pixels
-        height={200} // Intrinsic height of the image in pixels
+        src="/logo.png"
+        alt="App Logo"
+        width={300}
+        height={200}
+        priority // Add priority so the logo loads fast!
       />
       <Suspense fallback={<JoinCardSkeleton />}>
         <JoinCard />
